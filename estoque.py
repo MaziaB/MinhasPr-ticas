03fb1942640fb7359e7b38d713986207df1bd96e
Bruno <brunommbpro@gmail.com>
2025-11-10 14:55:04 -0300

    enunciado exercício estoque

---
+++ estoque.py
@@ -0,0 +1,23 @@
+# 1- Menu - adicionar produto, listar produtos, remover produto, atualizar quantidade de produto e sair.
+# 2- Utilizar um dicionário para armazenar os produtos. Cada produto será uma chave no dicionário, e o valor
+# será outro dicionário contendo duas chaves: "quantidade" (quantidade disponível) e "preço".
+# 3- Ao selecionar 'adicionar produto', o programa deve pedir o nome do produto, quantidade e preço.
+# 4- Adicione o produto ao dicionário com a quantidade e preço informados.
+# 5- Ao selecionar 'listar produtos', exiba todos os produtos no formato:
+# - "Nome do produto: quantidade disponível - preço";
+# - Os produtos devem ser exibidos em ordem alfabética (use uma função lambda para ordenar os produtos);
+# - Ao selecionar 'remover produto', peça o nome do produto a ser removido. Verifique se o produto existe
+# no dicionário antes de removê-lo. Caso o produto não exista, exiba uma mensagem de erro;
+# - Ao selecionar 'atualizar quantidade de produto', o programa deve pedir o nome do produto e a 
+# nova quantidade. Se o produto existir, a quantidade deve ser atualizada. Caso contrário, exiba uma
+# mensagem de erro.
+# - O programa deve rodar em loop até o usuário escolher a opção de sair.
+# - Organize seu código utilizando funções. Cada funcionalidade do menu deve ser implementada como uma
+# função separada, como por exemplo, uma função para adicionar produtos, outra para listar...
+# - Utilize operadores condicionais e laços de repetição para manter o programa funcional e simples.
+# 6- Exemplo do menu:
+# - Adicionar produto;
+# - Listar produtos;
+# - Remover produto;
+# - Atualizar quantidade de produto;
+# - Sair.
